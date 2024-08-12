e1ff8a9fec8133ade82be9126e7da36e4c92a92f
serhas <serhas@gmail.com>
2024-08-12 22:36:57 +0300

    mix added.

--- src/templates.rs
+++ src/templates.rs
@@ -4,19 +4,32 @@
 
 use halo2::halo2curves::bn256::Fr;
 
-// Exp of S-box.
+/// Exp of S-box.
 pub fn sigma(item: Fr) -> Fr {
     let item_2 = item * item;
     let item_4 = item_2 * item_2;
     item * item_4
 }
 
-// Adds round constants.
-pub fn ark(input: [Fr; 3], t: usize, c: Vec<Fr>, r: usize) -> [Fr; 3] {
+/// Adds round constants.
+pub fn ark(inputs: [Fr; 3], t: usize, c: Vec<Fr>, r: usize) -> [Fr; 3] {
     let mut state = [Fr::zero(); 3];
 
     for i in 0..t {
-        state[i] = input[i] + c[i + r]
+        state[i] = inputs[i] + c[i + r]
     }
+
     state
+}
+
+pub fn mix(state: [Fr; 3], t: usize, m: [[Fr; 3]; 3]) -> [Fr; 3] {
+    let mut new_state = [Fr::zero(); 3];
+
+    for i in 0..t {
+        for j in 0..t {
+            new_state[i] += m[j][i] * state[j];
+        }
+    }
+
+    new_state
 }
