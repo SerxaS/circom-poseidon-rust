1984e79cb863becd268ffb8d8a4aea71033a4419
serhas <serhas@gmail.com>
2024-08-15 09:24:20 +0300

    Templates updated.

--- src/gadgets/templates.rs
+++ src/gadgets/templates.rs
@@ -15,7 +15,7 @@ pub fn sigma(item: Fr) -> Fr {
 }
 
 /// Adds round constants.
-pub fn ark(state: Vec<Fr>, t: usize, c: Vec<Fr>, r: usize) -> Vec<Fr> {
+pub fn ark(state: &Vec<Fr>, t: usize, c: Vec<Fr>, r: usize) -> Vec<Fr> {
     let mut new_state = vec![Fr::zero(); t];
 
     for i in 0..t {
@@ -26,7 +26,7 @@ pub fn ark(state: Vec<Fr>, t: usize, c: Vec<Fr>, r: usize) -> Vec<Fr> {
 }
 
 /// Computes MDS matrix for MixLayer operation.
-pub fn mix(state: [Fr; 3], t: usize, m: [[Fr; 3]; 3]) -> Vec<Fr> {
+pub fn mix(state: Vec<Fr>, t: usize, m: [[Fr; 3]; 3]) -> Vec<Fr> {
     let mut new_state = vec![Fr::zero(); t];
 
     for i in 0..t {
@@ -52,7 +52,7 @@ pub fn poseidon_ex(inputs: [Fr; 2], initial_state: Fr, n_outs: usize) {
     let p = poseidon_p();
 
     let mut component_ark = vec![vec![Fr::zero(); t]; n_rounds_f];
-    let component_sigma_f = vec![vec![Fr::zero(); t]; n_rounds_f];
+    let mut component_sigma_f = vec![vec![Fr::zero(); t]; n_rounds_f];
     let component_sigma_p = vec![Fr::zero(); n_round_p];
     let component_mix = vec![Fr::zero(); n_rounds_f - 1];
     let component_mix_s = vec![Fr::zero(); n_round_p];
@@ -67,6 +67,17 @@ pub fn poseidon_ex(inputs: [Fr; 2], initial_state: Fr, n_outs: usize) {
             state[j] = initial_state;
         }
     }
+    
+    component_ark[0] = ark(&state, t, c, 0);    
+    
+    for r in 0..(n_rounds_f / 2) - 1 {
+        for j in 0..t {
+            if r == 0 {
+                component_sigma_f[r][j] = sigma(component_ark[0][j])
+            }
+            
+        }
+    }
 
-    component_ark[0] = ark(state, t, c, 0);    
+    println!("{:#?}", component_ark[0]);
 }
